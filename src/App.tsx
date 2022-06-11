377030d9f0ac0702504c1795f1e63aab250c1006
Pablo Alan <pabloxt14@gmail.com>
2022-06-11 18:22:26 -0300

    feat: handleMakeQuestion finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { PageContent } from "./components/PageContent";
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
 
   return (
     <div className="App">
       <PageContent />
+      <ToastContainer />
     </div>
   )
 }
--- src/components/PageContent/index.tsx
+++ src/components/PageContent/index.tsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import styles from './styles.module.css';
 
+import androidImg from '../../assets/android-gif.gif';
+import { toast } from 'react-toastify';
 
 const ANSWERS = [
     "Certeza!",
@@ -25,48 +27,69 @@ const ANSWERS = [
 ]
 
 export function PageContent() {
-    const answer = useRef<HTMLParagraphElement>(null!);
-
+    const answerElement = useRef<HTMLParagraphElement>(null!);
+    const inputQuestion = useRef<HTMLInputElement>(null!);
+    const buttonQuestion = useRef<HTMLButtonElement>(null!);
 
     function handleMakeQuestion() {
+
+        if (inputQuestion.current.value.trim() == "") {
+            toast.warning("Digite sua pergunta!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+            })
+            return;
+        }
+
+        buttonQuestion.current.setAttribute("disabled", "true");
+
+        const question = `<div>${inputQuestion.current.value}</div>`
+
         // Gerar numero aleatório
         const totalAnswers = ANSWERS.length;
         const randomNumber = Math.floor(Math.random() * totalAnswers);
+        answerElement.current.innerHTML = question + ANSWERS[randomNumber];
+        answerElement.current.style.opacity = "1";
 
-        const newAnswer = ANSWERS[randomNumber];
-
-        answer.current.innerHTML = newAnswer;
+        // Sumir a reposta depois de 3 segundos
+        setTimeout(() => {
+            answerElement.current.style.opacity = "0";
+            buttonQuestion.current.removeAttribute("disabled");
+        }, 3000);
     }
 
 
     return (
         <div className={styles.container}>
-            <img 
-                src="https://gist.githubusercontent.com/maykbrito/0acdf4ce919838ffed50915a31fc5b23/raw/6f4dd01ec3116428ec4c99255944cb9ac7927590/cristal-ball.svg" 
-                alt="Imagem de uma bola de cristal" 
+            <img
+                src="https://gist.githubusercontent.com/maykbrito/0acdf4ce919838ffed50915a31fc5b23/raw/6f4dd01ec3116428ec4c99255944cb9ac7927590/cristal-ball.svg"
+                alt="Imagem de uma bola de cristal"
                 className={styles.image}
             />
             <h1 className={styles.title}>Vou revelar o seu destino!</h1>
             <p className={styles.subtitle}>
                 Clique em fazer pergunta para que seu destino seja revelado.
             </p>
-            <input 
+            <input
+                ref={inputQuestion}
                 type="text"
                 placeholder='Digite sua pergunta'
                 className={styles.input}
             />
-            <button 
+            <button
+                ref={buttonQuestion}
                 className={styles.button}
                 onClick={handleMakeQuestion}
             >
                 Fazer Pergunta
             </button>
 
-            <h3 className={styles.answer}>
-                <div>Pergunta que fiz</div>
-                <p ref={answer}>
-
-                </p>
+            <h3 className={styles.answer} ref={answerElement}>
             </h3>
         </div>
     );
